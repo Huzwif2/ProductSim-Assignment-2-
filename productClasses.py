b7ce5eebfd6ebb5b48cda3291c2c342fba0590f2
Huzayfa Syed <huzayfa.syed@gmail.com>
2023-10-24 20:16:33 -0400

    Wrote initializer and defined parameters

--- productClasses.py
+++ productClasses.py
@@ -1,1 +1,21 @@
-print(":3")
+from random import randint
+
+class Product_Program():
+
+    def __init__(self, code, name, stock, price, cost, units):
+        self.code = code
+        self.name = name
+        self.stock = stock
+        self.price = price
+        self.cost = cost
+        self.manufactured_units = units
+        
+def get_user_info():
+    code = int(input("Please enter the Product Code (Between 100 to 1000): "))
+    name = input("Please enter the Product Name: ")
+    stock = int(input("Please enter the Current Stock: "))
+    price = float(input("Please enter the Product Sale Price: "))
+    cost = float(input("Please enter the Product Manufacture Cost: "))
+    units = int(input("Please enter Estimated Monthly Production: "))
+    print(code, name, stock, price, cost, units)
+get_user_info()
