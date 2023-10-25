c1a7ce3c0309bf1b830e07eabcdc65eb063bf910
Huzayfa Syed <huzayfa.syed@gmail.com>
2023-10-24 22:01:41 -0400

    Optimized user input. Validates and checks for type errors.

--- productClasses.py
+++ productClasses.py
@@ -9,13 +9,25 @@ class Product_Program():
         self.price = price
         self.cost = cost
         self.manufactured_units = units
-        
+
 def get_user_info():
-    code = int(input("Please enter the Product Code (Between 100 to 1000): "))
-    name = input("Please enter the Product Name: ")
-    stock = int(input("Please enter the Current Stock: "))
-    price = float(input("Please enter the Product Sale Price: "))
-    cost = float(input("Please enter the Product Manufacture Cost: "))
-    units = int(input("Please enter Estimated Monthly Production: "))
-    print(code, name, stock, price, cost, units)
+    try:
+        code = int(input("Please enter the Product Code (Between 100 to 1000): "))
+        name = input("Please enter the Product Name: ")
+        stock = int(input("Please enter the Current Stock: "))
+        price = float(input("Please enter the Product Sale Price: "))
+        cost = float(input("Please enter the Product Manufacture Cost: "))
+        units = int(input("Please enter Estimated Monthly Production: "))
+
+        # Validate user input
+        if 100 <= code <= 1000 and stock > 0 and price > 0 and cost > 0:
+            print("Oki")
+            print(code, name, stock, price, cost, units)
+        else:
+            print("One or more of the values entered is incorrect. Please try again.")
+            get_user_info()
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        get_user_info()
+        
 get_user_info()
